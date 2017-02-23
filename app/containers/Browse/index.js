5af379560a4f94d8d0f799debb745539660efe2a
theClubhouse <git@theclubhou.se>
2017-02-23 14:56:47 -0500

    updated the browse page to contain dummy data

--- app/containers/Browse/index.js
+++ app/containers/Browse/index.js
@@ -11,14 +11,41 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 export class Browse extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+  constructor(props){
+    super(props);
+    this.state = {
+      recipes:[
+        {Dish:"chicken poopasaurus1", Author: "Jane Doe", Difficulty: 2, Time_Req: "3 hours"},
+        {Dish:"chicken poopasaurus2", Author: "Jane Doe", Difficulty: 2, Time_Req: "3 hours"},
+        {Dish:"chicken poopasaurus3", Author: "Jane Doe", Difficulty: 2, Time_Req: "3 hours"}
+      ]
+    }
+  };
+
   render() {
+    const tableStyle = {
+      background: "rgba(0,0,0,.5)",
+      color: "rgb(255,205,30)",
+      margin: "auto",
+      width:"90%",
+      height: "50vh",
+      textAlign: "center",
+      marginBottom: "100px",
+      marginTop: "50px"
+    };
+
+    const rowStyle = {
+      border: "1px solid rgb(255,205,30)"
+    };
 
     const containerStyle = {
       background: "url(http://h4z.it/Image/7210d0_-1002897_640.jpg)",
       minHeight: "100vh",
+      maxHeight: "100vh",
+      overflow: "auto",
       width: "auto",
       backgroundRepeat: "repeat"
-
     };
 
     return (
@@ -31,7 +58,24 @@ export class Browse extends React.PureComponent { // eslint-disable-line react/p
         />
         <Header />
 
-        <main></main>
+        <main>
+          <table style={tableStyle}>
+            <tr style={rowStyle}>
+              <th>Dish</th>
+              <th>Author</th>
+              <th>Difficulty</th>
+              <th>Time req.</th>
+            </tr>
+            {this.state.recipes.map((recipe,i) => (
+              <tr style={rowStyle} key={i}>
+                <td>{recipe.Dish}</td>
+                <td>{recipe.Author}</td>
+                <td>{recipe.Difficulty}</td>
+                <td>{recipe.Time_Req}</td>
+              </tr>
+            ))}
+          </table>
+        </main>
 
         <Footer />
       </div>
